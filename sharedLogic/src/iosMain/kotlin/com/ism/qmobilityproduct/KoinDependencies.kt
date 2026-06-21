02d91fb884cc7651bfa10dca342add49196cb3af
ik24 <ikismailkhan024@gmail.com>
2026-06-22 02:24:43 +0400

    ios koin dependencies

---
+++ sharedLogic/src/iosMain/kotlin/com/ism/qmobilityproduct/KoinDependencies.kt
@@ -0,0 +1,23 @@
+package com.ism.qmobilityproduct
+
+import com.ism.qmobilityproduct.data.IosDatabaseDriverFactory
+import com.ism.qmobilityproduct.data.local.DatabaseDriverFactory
+import com.ism.qmobilityproduct.di.initKoin
+import com.ism.qmobilityproduct.presentation.DetailViewModel
+import com.ism.qmobilityproduct.presentation.FavouritesViewModel
+import com.ism.qmobilityproduct.presentation.ListViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+import org.koin.dsl.module
+
+fun initKoinIos() {
+    initKoin(listOf(module {
+        single<DatabaseDriverFactory> { IosDatabaseDriverFactory() }
+    }))
+}
+
+class KoinDependencies : KoinComponent {
+    val listViewModel: ListViewModel get() = get()
+    val detailViewModel: DetailViewModel get() = get()
+    val favouritesViewModel: FavouritesViewModel get() = get()
+}
