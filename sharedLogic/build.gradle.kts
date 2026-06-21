030466aebd4cd28e1509572ce984a0f3a8b6df9b
ik24 <ikismailkhan024@gmail.com>
2026-06-21 09:17:26 +0400

    init and dependencies

---
+++ sharedLogic/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidMultiplatformLibrary)
+    alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.skie)
+}
+
+kotlin {
+    androidLibrary {
+       namespace = "com.ism.qmobilityproduct.sharedLogic"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+
+        androidResources {
+            enable = true
+        }
+    }
+
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "SharedLogic"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+        commonMain.dependencies {
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.koin.core)
+            api(libs.lifecycle.viewmodel)
+        }
+    }
+}
---
+++ sharedLogic/src/commonMain/kotlin/com/ism/qmobilityproduct/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.ism.qmobilityproduct
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
